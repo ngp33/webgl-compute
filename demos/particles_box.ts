a1a5a4034e215204afc98a82d4fd8c6eb6113ec6
Mathias Madsen Stav <mathias@feed.no>
2024-05-16 13:53:05 +0200

    Update particles_box demo
    More performant draw calls, wrapping particles instead of colliding with walls. canvas fills the page.

--- demos/particles_box.ts
+++ demos/particles_box.ts
@@ -1,5 +1,6 @@
 import { init } from "../webgl_compute.js";
 
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const gl = canvas.getContext("webgl2");
 
@@ -9,13 +10,18 @@ if (gl === null) {
   );
   throw "";
 }
-
+const WIDTH = window.innerWidth
+const HEIGHT = window.innerHeight
+const HALF_WIDTH = WIDTH * 0.5
+const HALF_HEIGHT = HEIGHT * 0.5
+const PARTICLE_RADIUS = 1
 const glComp = init(gl);
+const displayCanvas = document.getElementById("display") as HTMLCanvasElement
+const displayCtx = displayCanvas.getContext("2d")!;
+displayCanvas.width = WIDTH;
+displayCanvas.height = HEIGHT;
 
-const displayCtx = (
-  document.getElementById("display") as HTMLCanvasElement
-).getContext("2d")!;
-displayCtx.scale(10, 10);
+displayCtx.scale(1, 1);  // We can comine this with pixelDensity if we like to get retina resolution
 
 const NUM_PARTICLES = 2000;
 
@@ -28,14 +34,15 @@ let posFBO = glComp.createFBO(
     new Array(NUM_PARTICLES)
       .fill(0)
       .map((v, i) => [
-        45.0 + Math.random() * 10.0,
-        45.0 + Math.random() * 10.0,
+        Math.random() * (WIDTH * 0.1) + HALF_WIDTH,
+        Math.random() * (HEIGHT* 0.1) + HALF_HEIGHT,
         0.0,
         0.0,
       ])
       .flat()
   )
 );
+
 let newPosFBO = glComp.createFBO(NUM_PARTICLES, 1, 4, "f32");
 let velFBO = glComp.createFBO(
   NUM_PARTICLES,
@@ -61,39 +68,61 @@ const updateVel = glComp.createComputation(
   `
 vec4 pos = ${glComp.MACROS.fbo_idx_myxy("pos")};
 fragColor = ${glComp.MACROS.fbo_idx_myxy("vel")};
-fragColor.x *= float(pos.x > 0.0 && pos.x < 100.0) * 2.0 - 1.0;
-fragColor.y *= float(pos.y > 0.0 && pos.y < 100.0) * 2.0 - 1.0;
+// Commenting out this, Lets wrap instead
+// fragColor.x *= float(pos.x > 0.0 && pos.x < ${WIDTH}.0) * 2.0 - 1.0;
+// fragColor.y *= float(pos.y > 0.0 && pos.y < ${500}.0) * 2.0 - 1.0;
+
   `
 );
 const updatePos = glComp.createComputation(
   { pos: "fbo", vel: "fbo", dt: "float" },
   `
-fragColor = clamp(${glComp.MACROS.fbo_idx_myxy(
-    "pos"
-  )} + ${glComp.MACROS.fbo_idx_myxy("vel")} * dt, 0.0, 100.0);
+  vec4 pos = ${glComp.MACROS.fbo_idx_myxy("pos")};
+  vec4 vel = ${glComp.MACROS.fbo_idx_myxy("vel")};
+  
+  fragColor.x = clamp(pos.x + vel.x * dt, 0.0, ${WIDTH}.0);
+  fragColor.y = clamp(pos.y + vel.y * dt, 0.0, ${HEIGHT}.0);
+  
+  // Wrapping
+  // Check if particle is out of screen. 
+  if(fragColor.y >= ${HEIGHT}.0) {
+    fragColor.y = 1.0;
+  }
+  if(fragColor.y <= 0.0) {
+    fragColor.y = ${HEIGHT-1}.0;
+  }
+  if(fragColor.x >= ${WIDTH}.0) {
+    fragColor.x = 1.0;
+  }
+  if(fragColor.x <= 0.0) {
+    fragColor.x = ${WIDTH-1}.0;
+  }
+
   `
 );
 
 const drawOutput = () => {
   const posOut = glComp.readFBORaw(posFBO);
   const velOut = glComp.readFBORaw(velFBO);
-
-  displayCtx.clearRect(0, 0, 1000, 1000);
-
+  const particleSize = PARTICLE_RADIUS * 2
+  displayCtx.clearRect(0, 0, WIDTH, HEIGHT);
+  displayCtx.fillStyle = "black";
   for (let i = 0; i < NUM_PARTICLES; i++) {
     const [x, y] = [posOut[i * 4], posOut[i * 4 + 1]];
     const [vx, vy] = [velOut[i * 4], velOut[i * 4 + 1]];
-
+    displayCtx.fillRect(x, y, particleSize, particleSize)
+    // displayCtx.beginPath();
+    // displayCtx.arc(x, y, 0.5, 0, Math.PI * 2);
+    // displayCtx.fillStyle = "green";
+    // displayCtx.fill();
+    // displayCtx.closePath();
     displayCtx.beginPath();
-    displayCtx.arc(x, y, 0.5, 0, Math.PI * 2);
-    displayCtx.fillStyle = "green";
-    displayCtx.fill();
-    displayCtx.closePath();
-    displayCtx.beginPath();
-    displayCtx.moveTo(x, y);
-    displayCtx.lineTo(x + vx * 20, y + vy * 20);
-    displayCtx.strokeStyle = "black";
-    displayCtx.lineWidth = 0.2;
+    const cx = x +PARTICLE_RADIUS
+    const cy = y +PARTICLE_RADIUS
+    displayCtx.moveTo(cx, cy);
+    displayCtx.lineTo(x + vx * 100, y + vy * 100);
+    displayCtx.strokeStyle = "red";
+    displayCtx.lineWidth = 0.5;
     displayCtx.stroke();
     displayCtx.closePath();
   }
@@ -126,3 +155,5 @@ const mainLoop = (t: number) => {
 };
 
 mainLoop(0);
+
+
